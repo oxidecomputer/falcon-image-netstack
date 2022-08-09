d19e9aec238e4c2dd6a41dbe8aa85e1f74528e5c
Levon Tarver <levon@oxide.computer>
2022-08-09 16:40:26 -0500

    First draft of running halfstack in ci

---
+++ .github/buildomat/jobs/netstack-prepare.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+# This script is used to prepare the tools used in the
+# netstack validation environment
+#:
+#: name = "netstack-prepare"
+#: variety = "basic"
+#: target = "helios"
+#: rust_toolchain = "nightly"
+#:
+#: output_rules = [
+#:   "=/out/build-tools.tar.xz",
+#: ]
+#:
+#: access_repos = [
+#:   "oxidecomputer/falcon-image-netstack",
+#:   "oxidecomputer/falcon",
+#:   "oxidecomputer/propolis",
+#:   "oxidecomputer/testbed",
+#: ]
+#:
+#: [[publish]]
+#: series = "tools"
+#: name = "build-tools.tar.xz"
+#: from_output = "/out/build-tools.tar.xz"
+#:
+#: [dependencies.build]
+#: job = "netstack-image"
+#
+
+set -e
+set -o pipefail
+set -o xtrace
+
+export RUST_LOG=debug
+
+# List the host's interfaces
+dladm show-link
+topdir=$(pwd)
+
+mkdir bin
+
+pushd bin
+curl -OL https://github.com/stedolan/jq/releases/download/jq-1.4/jq-solaris11-64
+mv jq-solaris11-64 jq
+chmod +x jq
+popd
+
+#
+# Shim calls to github to force https instead of ssh for cloning
+#
+if [[ -n $CI ]]; then
+    source ./evil-clone-hack.sh
+fi
+
+#
+# Clone the falcon repo.
+# We need the falcon tooling to install the images needed for our falcon
+# topologies.
+#
+if [[ ! -d falcon ]]; then
+    git clone https://github.com/oxidecomputer/falcon.git
+fi
+
+#
+# Clone the testbed repo
+# We need the testbed repo because it contains the particular topology we'll be running
+# in our CI task
+#
+if [[ ! -d testbed ]]; then
+    git clone https://github.com/oxidecomputer/testbed.git
+fi
+
+#
+# Build the halfstack-2x2-ci falcon topology binary for use in our next CI task
+#
+pushd testbed/halfstack-2x2-ci
+cargo build
+cp ../target/debug/halfstack-2x2-ci "$topdir/bin/"
+cargo clean
+popd
+
+#
+# Bundle everything up for use in the next CI task
+#
+pfexec mkdir -p /out
+pfexec chown -R "$UID" /out
+cd ..
+tar -cf - falcon-image-netstack | xz -T 0 -9 -vv -c - > /out/build-tools.tar.xz
---
+++ .github/buildomat/jobs/netstack-validation.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+# This script is used to validate the netstack image
+# It builds a falcon topology using the latest netstack image,
+# then tests communication across the simulated network fabric.
+#:
+#: name = "netstack-validation"
+#: variety = "basic"
+#: target = "lab"
+#: rust_toolchain = "nightly"
+#:
+#: access_repos = [
+#:   "oxidecomputer/falcon-image-netstack",
+#:   "oxidecomputer/falcon",
+#:   "oxidecomputer/propolis",
+#:   "oxidecomputer/testbed",
+#: ]
+#:
+#: skip_clone = true
+#:
+#: [dependencies.build]
+#: job = "netstack-prepare"
+#
+#
+
+set -e
+set -o pipefail
+set -o xtrace
+
+export RUST_LOG=debug
+
+#
+# This function is for convenience, since the falcon scripts rely on
+# aliases that are usually present on end-user machines, but not in CI
+#
+function sha256sum() {
+    command shasum -a 256 $*
+}
+
+export -f sha256sum
+
+#
+# Create etherstub for falcon topology to use as its external link
+#
+pfexec dladm create-etherstub falcon_stub0
+pfexec dladm create-vnic -l falcon_stub0 falcon_vnic0
+
+#
+# This interface is used for the falcon topology (external link)
+# It will be a part of a private network, we will use NAT to
+# facilitate communication with the outside world
+#
+export INTERFACE=falcon_stub0
+
+#
+# This interface is used to reach the internet
+#
+export EXT_INTERFACE=igb0
+
+#
+# Create a private ip address for the host to use for communication with the
+# falcon topology VMs
+#
+pfexec ipadm create-addr -T static -a 192.168.100.100/24 falcon_vnic0/internal
+
+#
+# Enable packet filtering
+#
+pfexec ipf -E
+
+#
+# Enable ipv4-forwarding on host so VM traffic can be forwarded to EXT_INTERFACE
+#
+routeadm -e ipv4-forwarding -u
+
+#
+# Enable ipfilter (NAT)
+#
+svcadm enable -s ipfilter
+
+#
+# Check status of services
+#
+svcs -x ipv4-forwarding
+svcs -x route
+svcs -x ipfilter
+
+#
+# Add NAT rules for outbound traffic from our private network
+# We must place a configuration file in /etc/ipf/ipnat.conf, otherwise the
+# ipfilter service may randomly flush our rules
+#
+svccfg -s ipfilter:default setprop firewall_config_default/policy = astring: "custom"
+echo "map $EXT_INTERFACE 192.168.100.0/24 -> 0/32 portmap tcp/udp 1025:65000" >> ipnat.conf
+echo -n "map $EXT_INTERFACE 192.168.100.0/24 -> 0/32 portmap" >> ipnat.conf
+pfexec cp ipnat.conf /etc/ipf/ipnat.conf
+pfexec ipnat -f /etc/ipf/ipnat.conf
+
+#
+# Verify presence of NAT rules
+#
+pfexec ipnat -l
+
+#
+# Extract the tools needed for launching the falcon topology
+#
+mkdir /tmp/netstack-validation
+pushd /tmp/netstack-validation
+xzcat /input/build/out/build-tools.tar.xz | tar -xf -
+
+#
+# Add jq and halfstack-2x2-ci to our PATH
+#
+pushd falcon-image-netstack
+export PATH="$(pwd)/bin:$PATH"
+
+pushd falcon
+
+#
+# Create the zpool used for extracting our falcon topology images
+#
+pfexec zpool create -f netstack-validation c1t1d0
+export FALCON_DATASET=netstack-validation/falcon
+
+#
+# Install tooling and images needed for the falcon topology
+#
+./get-propolis.sh
+./get-ovmf.sh
+./get-netstack.sh
+
+popd
+
+#
+# Set the version of netstack to user
+#
+VERSION=$(cat version.txt)
+export IMAGE=${VERSION%_*}
+
+#
+# Run the test
+#
+pushd testbed/halfstack-2x2-ci
+pfexec halfstack-2x2-ci launch
